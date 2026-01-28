881538497f5a497c502e67e43432f3aca1a8c927
Armored Dragon <publicmail@armoreddragon.com>
2026-01-28 16:28:47 -0600

    Moved credential_store.

--- src/scripts/utils/credential_store.gd
+++ src/scripts/utils/credential_store.gd
@@ -2,7 +2,6 @@
 # This script is more of a placeholder until a more secure method of storing these credentials becomes available.
 # Due to security issues revolving around how Godot handles scripts umong other things, any object can be made accessible by anything else.
 # As a result, there just isn't a safe spot to store this data.
-
 extends Node
 
 var info = {
@@ -14,3 +13,6 @@ func set_account_credential(credentials: PackedStringArray = []):
     info.token = credentials[0]
     info.expire_time = credentials[1]
     GlobalLogger.logs("Saved JWT to memory.")
+
+# TODO: Save account credentials to disk.
+# TODO: Read account credentials from disk.
