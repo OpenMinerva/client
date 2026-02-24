db76856468b650dfc7e0b0b87f6cff7574328ead
Armored Dragon <publicmail@armoreddragon.com>
2026-02-23 21:21:06 -0600

    Remove dead code.

--- src/userinterface/signin.gd
+++ src/userinterface/signin.gd
@@ -91,75 +91,9 @@ func _create_account() -> void:
 
 	if local_account == false && account_server != "":
 		account_lib.authenticate(local_id, password, remember_me)
-		# _connect_to_account_server(local_id, username, password, account_server, device_keys.public, remember_me)
 
 	_render_account_list()
 	_change_primary_view("AccountList")
-	return
-
-func _connect_to_account_server(local_id: String, username: String, password: String, account_server: String, public_device_key: String, remember_me: bool = false):
-	# TODO: Invalid passwords.
-	# TODO: Account not found.
-	# TODO: Account server unreachable.
-	GlobalLogger.logs("Attempting to authorize the connection to the account server.")
-
-	var data = {"username": username, "password": password, "pubKey": public_device_key}
-	var response = await http.req(HTTPClient.Method.METHOD_POST, account_server, "/api/v1/login", 40400, ["Accept: application/json", "Content-Type: application/json"], JSON.stringify(data))
-	
-	# Get the private JWT for use between this device and the account server only.
-	if response["ok"] == false:
-		GlobalLogger.logs("Response failed for unknown reason.", 3)
-		return
-
-	if response["body"] == null:
-		GlobalLogger.logs("No body provided for login request.", 3)
-		return
-
-	var res_body = JSON.parse_string(response["body"])
-
-	if "error" in res_body.keys():
-		GlobalLogger.logs("Login request returned an error. '%s'" % res_body["error"], 1)
-		return
-
-	var private_account_server_jwt = response["response_headers"]["Set-Cookie"].split("; ")
-	private_account_server_jwt[0] = private_account_server_jwt[0].replace("token=", "")
-	GlobalLogger.logs("Successfully authorized with the account server.", 1)
-
-	# Get the public passport clients will send to everyone excluding the account server.
-	# We use two "tokens" so that one can make actions on behalf of an account, 
-	# and this passport just to validate we are who we say we are. This token should not be used to make any actions to the account.
-	GlobalLogger.logs("Attempting to authorize this device with the provided account.")
-	var device_response = await http.req(HTTPClient.Method.METHOD_POST, account_server, "/api/v1/device/auth", 40400, ["Accept: application/json", "Content-Type: application/json"], JSON.stringify(data))
-	
-	if device_response["ok"] == false:
-		GlobalLogger.logs("Authorizing this device failed for unknown reason.", 1)
-		return
-
-	if device_response["body"] == null:
-		GlobalLogger.logs("No body provided for device authorization request.", 3)
-		return
-
-	var device_response_body = JSON.parse_string(device_response["body"])
-
-	if "error" in device_response_body.keys():
-		GlobalLogger.logs("Device authorization request returned an error. '%s'" % res_body["error"], 1)
-		return
-
-	var public_account_server_passport = device_response["response_headers"]["Set-Cookie"].split("; ")
-	public_account_server_passport = public_account_server_passport[0].replace("token=", "")
-
-	# Store the tokens in memory
-	CredentialStore.set_public_account_server_passport(public_account_server_passport)
-	CredentialStore.set_private_account_server_jwt(private_account_server_jwt)
-
-	if remember_me:
-		# Persist if we want to be remembered.
-		var _account_config = await FileManager.read_config_file("accounts", "accounts_db.cfg")
-		_account_config.set_value(local_id, "private_account_server_jwt", private_account_server_jwt)
-		_account_config.set_value(local_id, "public_account_server_passport", public_account_server_passport)
-		_account_config.save(ACCOUNT_DATABASE_DIRECTORY)
-		GlobalLogger.logs("Saved the private JWT and the passport to the account database.", 1)
-
 	return
 
 # Buttons:
