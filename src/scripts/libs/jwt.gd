6ddd2b1c8919ae8dc8df92d435b2253b159f3062
Armored Dragon <publicmail@armoreddragon.com>
2026-02-24 00:42:58 -0600

    Created placeholder jwt lib file.

---
+++ src/scripts/libs/jwt.gd
@@ -0,0 +1,18 @@
+extends Node
+
+# NOTE: Currently the account server provides JWT that are Base64url encoded. We want to change this to Base64 in this application.
+
+func decode():
+    return
+
+func encode():
+    return
+
+func validate():
+    return
+
+func base64url_to_base64():
+    return
+
+func base64_to_base64url():
+    return
