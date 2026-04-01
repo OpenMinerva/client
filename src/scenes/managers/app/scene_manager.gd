05e027a15f7caf178a4fccf940704a6584f43271
Armored-Dragon <publicmail@armoreddragon.com>
2026-03-31 20:27:17 -0500

    Multi Account support.
    * Base work for libraries.
    
    * Remove dead code.
    
    * Account authentication status.
    
    * Passport handling.
    Changes how passports are obtained.
    Select an account to use.
    Remove dead code.
    
    * Dashboard current account selection.
    
    * Created placeholder jwt lib file.
    
    * Changed how responses are verified.
    Fixed passport sending to server.
    Fixed timing issue requesting passport from account server.
    
    * Change the dashboard to something more presentable. (#64)
    
    * New dashboard work.
    
    * Changed sessions icon.
    Added instance option to manage the current instance.
    
    * Disable dashboard buttons when page is not present.
    
    * Storage display.
    
    * Add account list page.
    Improved display_dashboard... function.
    
    * Created placeholder theme for panels.
    
    * Basic account list haneling.
    
    * Better create account functionality.
    
    * Account login.
    
    * Replaced original dashboard.
    Scroll to bottom of messages when contacts are open.
    
    * Use OAuth authentication for the game client. (#67)
    
    * Initial work.
    Lots of hard coded values, but this is functional ground work for OAuth support from the game client.
    
    * Generate a random key for the pkce.
    
    * Made the auth code non-global.
    
    * Refresh token and access token.
    
    * Added TODOs.
    
    * Added file header.
    psioniq File Header extension config.
    
    * Reimplemented JWT library.
    
    * Set oauth variables.
    
    * Removed hardcoded authentication server values.
    
    * Use existing random_string in random library.
    
    * Base64 to Base64uri function in jwt library.
    
    * Token introspection and refreshing.
    
    * Placeholder TCPResponse to successful token capture.
    The browser will no longer hang continuiously.
    
    * Cleanup.
    
    * Added multiple login options.
    This changes the login flow to have the user specify which login they are intending on using.
    
    * Created url library.
    This will centralize url parsing making it easier to handle urls.
    
    * Removed passport handling.
    This will likely be reimplemented later.
    
    * Improved create account flow.
    
    * Disabled usernamepassword login.
    
    * Use URL Parser library.
    
    * Removed unused AES file.
    Right now I do not need this here.
    
    * Removed duplicate library.
    Removed unused references.
    
    * Stop checking OAuth server after successful connection.
    
    * Change the dashboard handling to use a singal bus. (#78)
    
    * Dev upload public key to the server.
    I need to make some changes to the UIX so I am making a checkpoint here.
    
    * Change structure of hut to use signalbus.
    
    * OAuth flow.
    
    * Go back from account creation page.
    
    * Refresh account list on change.
    
    * Move to account list after creating a local account.
    
    * Check to see if account is valid before logging in.
    
    * Remove unused account data display.
    
    * Disable navigation to unknown pages.
    
    * Exit page.
    
    * Removed old dash files.

--- src/scenes/managers/app/scene_manager.gd
+++ src/scenes/managers/app/scene_manager.gd
@@ -13,7 +13,7 @@ var server_init: bool = false
 
 func _ready():
 	await network_manager.start_server()
-	var session_name = Random.random_string(6)
+	var session_name = Random.random_string(6, true)
 	var new_home = player_home_scene.instantiate()
 	new_home.name = session_name
 	network_manager.active_session = session_name
---
+++ src/userinterface/dash/home.gd
@@ -0,0 +1,37 @@
+# --- License
+# File: /client/src/userinterface/dash/home.gd
+# Project: OpenMinerva
+# Created Date: 27 March 2026
+# Copyright (c) 2026 OpenMinerva
+# License: MIT License
+# Authors: Armored Dragon
+# --- License
+
+extends Control
+
+@onready var account_card_container = get_node("HBoxContainer/VBoxContainer/AccountDisplay")
+@onready var storage_card_container = get_node("HBoxContainer/VBoxContainer/StorageDisplay")
+@onready var session_card_container = get_node("HBoxContainer/VBoxContainer3/SessionDisplay")
+
+func _ready():
+	account_card_container.get_node("Button").pressed.connect(Events.emit_signal.bind("dash_switch_tab", "AccountDisplay"))
+	
+	Events.connect("dash_active_account_changed", _handle_active_account_changed)
+	Events.connect("dash_storage_changed", _handle_storage_changed)
+	Events.connect("dash_session_changed", _handle_session_changed)
+	
+	return
+
+func _handle_active_account_changed(account: Dictionary) -> void:
+	account_card_container.get_node("MarginContainer/HBoxContainer/VBoxContainer/Username").text = account.get("username") if account.get("username") else account.get("display_name")
+	account_card_container.get_node("MarginContainer/HBoxContainer/VBoxContainer/AccountServer").text = account.account_server
+	return
+
+func _handle_storage_changed(storage_data: Dictionary) -> void:
+	storage_card_container.get_node("MarginContainer/VBoxContainer/ProgressBar").value = storage_data.used_percent
+	storage_card_container.get_node("MarginContainer/VBoxContainer/Label2").text = "%s GiB used of %s GiB" % [storage_data.used_gigs, storage_data.total_gigs]
+	return
+
+func _handle_session_changed(session_data: Dictionary) -> void:
+	session_card_container.get_node("MarginContainer/HBoxContainer/VBoxContainer/SessionName").text = session_data.session_name
+	return
