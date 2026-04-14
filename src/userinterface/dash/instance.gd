c4637fc8e8d5b8f22e2f2d4592a33316184d2b01
Armored Dragon <publicmail@armoreddragon.com>
2026-04-14 15:24:49 -0500

    Removed unneeded Enum.

--- src/userinterface/dash/instance.gd
+++ src/userinterface/dash/instance.gd
@@ -9,14 +9,6 @@
 
 extends Control
 
-enum PrivacyLevel {
-	INVITE = 0,
-	PUBLIC = 1,
-	CONTACTS_PLUS = 2,
-	CONTACTS = 3,
-	FRIENDS_PLUS = 4,
-	FRIENDS = 5
-}
 
 @onready var network_m = get_tree().current_scene.get_node("NetworkManager")
 
@@ -29,16 +21,16 @@ enum PrivacyLevel {
 @onready var save_changes_btn = get_node("VBoxContainer/HBoxContainer2/SaveChanges")
 
 func _ready():
-	instance_privacy_public_btn.pressed.connect(_update_instance_privacy.bind(PrivacyLevel.PUBLIC))
-	instance_privacy_contacts_btn.pressed.connect(_update_instance_privacy.bind(PrivacyLevel.CONTACTS))
-	instance_privacy_friends_btn.pressed.connect(_update_instance_privacy.bind(PrivacyLevel.FRIENDS))
-	instance_privacy_invite_btn.pressed.connect(_update_instance_privacy.bind(PrivacyLevel.INVITE))
+	instance_privacy_public_btn.pressed.connect(_update_instance_privacy.bind(Enum.PrivacyLevel.PUBLIC))
+	instance_privacy_contacts_btn.pressed.connect(_update_instance_privacy.bind(Enum.PrivacyLevel.CONTACTS))
+	instance_privacy_friends_btn.pressed.connect(_update_instance_privacy.bind(Enum.PrivacyLevel.FRIENDS))
+	instance_privacy_invite_btn.pressed.connect(_update_instance_privacy.bind(Enum.PrivacyLevel.INVITE))
 
 	save_changes_btn.pressed.connect(_handle_save_session_info)
 
 	Events.connect("instance_updated", update_instance)
 
-	_update_instance_privacy(PrivacyLevel.INVITE)
+	_update_instance_privacy(Enum.PrivacyLevel.INVITE)
 	return
 
 func _update_instance_privacy(level):
@@ -51,13 +43,13 @@ func _update_instance_privacy(level):
 		if node is Button:
 			_privacy_button_disable(node)
 	match level:
-		PrivacyLevel.INVITE:
+		Enum.PrivacyLevel.INVITE:
 			_privacy_button_enable(instance_privacy_invite_btn)
-		PrivacyLevel.PUBLIC:
+		Enum.PrivacyLevel.PUBLIC:
 			_privacy_button_enable(instance_privacy_public_btn)
-		PrivacyLevel.CONTACTS:
+		Enum.PrivacyLevel.CONTACTS:
 			_privacy_button_enable(instance_privacy_contacts_btn)
-		PrivacyLevel.FRIENDS:
+		Enum.PrivacyLevel.FRIENDS:
 			_privacy_button_enable(instance_privacy_friends_btn)
 
 	return
