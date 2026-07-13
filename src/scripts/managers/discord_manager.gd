73ebf28b1148f1ac9f0b93d5df0c403128677257
Armored Dragon <publicmail@armoreddragon.com>
2026-07-13 14:22:13 -0500

    Added Discord RPC addon. Added basic Discord RPC functionality.

---
+++ src/scripts/managers/discord_manager.gd
@@ -0,0 +1,58 @@
+# --- License
+# File: /client/src/scripts/managers/discord_manager.gd
+# Project: OpenMinerva
+# Created Date: 13 June 2026
+# Copyright (c) 2026 OpenMinerva
+# License: MIT License
+# Authors: Armored Dragon
+# --- License
+class_name Discord
+extends Node
+
+static var _current_players: int = 0
+static var _max_players: int = 0
+
+static var _is_private: bool = true
+
+static func _init() -> void:
+	DiscordRPC.app_id = 1526274978486554684
+	DiscordRPC.large_image_text = "OpenMinerva"
+	DiscordRPC.large_image = "om-logo"
+
+	set_playercount(-1, -1)
+	set_world("Private")
+
+	DiscordRPC.start_timestamp = int(Time.get_unix_time_from_system())
+
+	DiscordRPC.refresh()
+	return
+
+static func set_playercount(current: int = -1, max: int = -1) -> void:
+	_current_players = current
+	_max_players = max
+	DiscordRPC.state = "%s / %s" % [_current_players, _max_players]
+	DiscordRPC.refresh()
+	return
+
+static func set_world(privacy: String = "Private", name: String = "Unnamed") -> void:
+	if privacy == "Public":
+		DiscordRPC.small_image_text = "Public world"
+		DiscordRPC.small_image = "public-1"
+		DiscordRPC.details = "In '%s'" % name
+	elif privacy == "Private":
+		DiscordRPC.small_image_text = "Private world"
+		DiscordRPC.small_image = "private-1"
+		DiscordRPC.details = "In a private world"
+
+	DiscordRPC.refresh()
+	return
+
+static func set_enabled(enabled: bool = true) -> void:
+	if enabled == true:
+		GlobalLogger.log("Enabling Discord rich presence.")
+		DiscordRPC.unclear()
+		_init()
+	else:
+		GlobalLogger.log("Disabling Discord rich presence.")
+		DiscordRPC.clear()
+	return
