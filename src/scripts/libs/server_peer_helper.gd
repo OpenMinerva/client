75d45d5b429e310ecb643ac6f98bd463b93f0399
Armored Dragon <publicmail@armoreddragon.com>
2026-07-26 16:57:12 -0500

    Moved some reused code to another sub-function.

--- src/scripts/libs/server_peer_helper.gd
+++ src/scripts/libs/server_peer_helper.gd
@@ -12,28 +12,23 @@ class_name ServerPeerHelper
 static var tree = Engine.get_main_loop() as SceneTree
 
 static func create_server(port: int, max_clients: int, master_scene_path: String) -> SceneMultiplayer:
-	var _session_api = SceneMultiplayer.new()
 	var _session_peer = ENetMultiplayerPeer.new()
-
 	var _create_response = _session_peer.create_server(port, max_clients)
 
-	if _create_response != OK:
-		GlobalLogger.log("Failed to create a server. Error: '%s'" % _create_response, Enum.LogLevel.INFO)
-		return null
-
-	# Set the multiplayer peer for the api to the ENetMultiplayer peer.
-	_session_api.multiplayer_peer = _session_peer
-
-	# Set the root path for the multiplayer api
-	tree.set_multiplayer(_session_api, master_scene_path)
+	var _session_api = _setup_peer(_create_response, _session_peer, master_scene_path)
 
 	return _session_api
 
 static func create_client(ip: String, port: int, master_scene_path: String) -> SceneMultiplayer:
-	var _session_api = SceneMultiplayer.new()
 	var _session_peer = ENetMultiplayerPeer.new()
-
 	var _create_response = _session_peer.create_client(ip, port)
+
+	var _session_api = _setup_peer(_create_response, _session_peer, master_scene_path)
+
+	return _session_api
+
+static func _setup_peer(_create_response, _session_peer, master_scene_path) -> SceneMultiplayer:
+	var _session_api = SceneMultiplayer.new()
 
 	if _create_response != OK:
 		GlobalLogger.log("Failed to create a client. Error: '%s'" % _create_response, Enum.LogLevel.INFO)
